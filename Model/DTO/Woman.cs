b065bbc7539fe04e8b3cbc6d817bb83c2ddafa77
Данил Тимофеев <danil2006timofeev@gmail.com>
2024-03-13 10:26:23 +0300

    af

--- Model/DTO/Woman.cs
+++ Model/DTO/Woman.cs
@@ -1,8 +1,17 @@
-﻿namespace WpfMvvmSample.Model.DTO
+﻿namespace WpfMvvmSample.Components.DTO
 {
-    public class Woman
+    public class Cpu
     {
         public string Name { get; set; }
-        public string Post { get; set; }
+        public int Cost { get; set; }
+        public int Frequency { get; set; }
+        public int Threads { get; set; }
+    }
+    public class Gpu
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string VideoMemory { get; set; }
+        public int Frequency { get; set; }
     }
 }
